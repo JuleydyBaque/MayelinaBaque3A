cbfe9674633c99aa4badea39d7e6ed40647ebf4e
JuleydyBaque <56653586+JuleydyBaque@users.noreply.github.com>
2019-10-16 15:09:16 -0500

    Add files via upload

---
+++ Ejercicio.9/Ejercicio.9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._9
+//Crear el programa tablaMultiplicar que escriba la tabla de multiplicar del número 1 al número 15. 
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" :::::: TABLA DE MULTIPLICAR DEL 1 AL 15 :::::: ");
+
+            for (int i = 1; i <= 15; i++)
+            {
+                Console.WriteLine("  ");
+                Console.WriteLine(i);
+
+                for (int j = 1; j <= 12; j++)
+                {
+
+                    Console.WriteLine(i + " X " + j + " = " + i * j);
+
+                }
+
+            }
+            Console.WriteLine("  ");
+            Console.WriteLine(":::::::PROGRAMA FINALIZADO:::::::");
+            Console.ReadKey();
+        }
+    }
+}
