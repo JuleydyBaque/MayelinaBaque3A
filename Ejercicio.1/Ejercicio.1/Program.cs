2edeea5e7b526c95877d3ca646934d99bf8efbbb
JuleydyBaque <56653586+JuleydyBaque@users.noreply.github.com>
2019-10-16 14:59:04 -0500

    Add files via upload

---
+++ Ejercicio.1/Ejercicio.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio._1
+//Programa que lea una serie de números por teclado e indique cuál es el mayor.
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cant, num;
+            int mayor = 0;
+            Console.WriteLine("Digite la cantidad de números a comparar");
+            cant = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cant; i++)
+            {
+                Console.WriteLine("Escriba un número");
+                num = int.Parse(Console.ReadLine());
+
+                if (i == 0)
+                {
+                    mayor = num;
+
+                }
+                else if (i != 0)
+                {
+                    if (num > mayor)
+                    {
+                        mayor = num;
+                    }
+                }
+
+            }
+            Console.WriteLine("El número mayor es:" + mayor);
+            Console.WriteLine("--------FIN DEL PROGRAMA-------");
+            Console.ReadKey();
+
+        }
+    }
+}
